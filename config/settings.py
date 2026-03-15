34fba0850c7cd8fc0c19e9542bf299357a13a0ff
Hanan-Abbas <hananabbas0101@gmail.com>
2026-03-15 21:44:22 +0500

    Update settings.py

---
+++ config/settings.py
@@ -0,0 +1,14 @@
+import os
+
+
+# Model Config
+EMBEDDING_MODEL = "all-MiniLM-L6-v2"  # Fast for local dev
+LLM_MODEL = "llama3" # Ensure you have this pulled in Ollama
+
+# Extraction Config
+SUPPORTED_EXTENSIONS = {'.py', '.js', '.ts', '.java', '.cpp', '.md', '.json', '.yml', '.yaml', '.dockerfile'}
+IGNORE_DIRS = {'.git', 'node_modules', '__pycache__', 'venv', 'dist', 'build', 'env'}
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GROQ_BASE_URL = "https://api.groq.com/openai/v1"
+LLM_MODEL = "llama-3.3-70b-versatile"
