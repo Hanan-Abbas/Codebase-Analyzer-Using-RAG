7861e713158b4581ea111e95593d7917c5c2fd91
Hanan-Abbas <hananabbas0101@gmail.com>
2026-03-16 21:27:47 +0500

    Update code_cleaner.py

--- src/services/processing_service/code_cleaner.py
+++ src/services/processing_service/code_cleaner.py
@@ -16,3 +16,9 @@ class CodeCleaner:
         content = "\n".join([line.rstrip() for line in content.splitlines()])
         
         return content.strip()
+
+    @staticmethod
+    def is_likely_binary(content):
+        """Check if a file is binary vs text."""
+        text_chars = bytearray({7, 8, 9, 10, 12, 13, 27} | set(range(0x20, 0x100)) - {0x7f})
+        return bool(content.translate(None, text_chars))
